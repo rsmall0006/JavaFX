5130d14a54d561663ce62d6e80b833dbf3330b3c
Ross Small <Merlin@Rosss-MacBook-Pro.local>
2017-03-05 22:15:35 -0500

    I have completed the JavaFX Contacts with SceneBuilder

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,63 @@
 package sample;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    ObservableList<Contact> items = FXCollections.observableArrayList();
+
+    @FXML
+    ListView list;
+
+    @FXML
+    TextField Name;
+
+    @FXML
+    TextField Phone;
+
+    @FXML
+    TextField Email;
+
+
+    @FXML
+    HBox text;
+
+
+    public void addItem(){
+        Contact contact = new Contact(Name.getText(), Phone.getText(), Email.getText());
+        items.add(contact);
+        Name.setText("");
+        Phone.setText("");
+        Email.setText("");
+
+//        items.add(new Contact(Name.getText()));
+//        Name.setText("");
+//        items.add(new Contact(Phone.getText()));
+//        Phone.setText("");
+//        items.add(new Contact(Email.getText()));
+//        Email.setText("");
+    }
+//    {System.out.println("addItem");}
+
+    public void removeItem (){
+        Contact contact = (Contact) list.getSelectionModel().getSelectedItem();
+        items.remove(contact);
+    }
+
+//    {System.out.println("removeItem");}
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        list.setItems(items);
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Contacts Desktop");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
@@ -20,4 +20,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
